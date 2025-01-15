d93e99d5ec1335706e1aa2a0faa0e756a17fd3a8
Arham Zahid <arham.zahid@kcl.ac.uk>
2025-01-15 22:02:49 +0000

    Fixed references from my_project to universal_society

--- universal_society/asgi.py
+++ universal_society/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for my_project project.
+ASGI config for universal_society project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'universal_society.settings')
 
 application = get_asgi_application()
