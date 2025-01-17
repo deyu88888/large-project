f52c7a33a92ec241c2d0e586a57eab07f96d9e06
m4nusha <manusharavindrathas@gmail.com>
2025-01-17 22:38:10 +0000

    Add CurrentUser endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
-    #path("register/", RegisterView.as_view(), name="register"),
-    path('/api/user/register', RegisterView.as_view(), name="register"),
+    path("user/token/refresh", TokenRefreshView.as_view(), name="refresh"),
+    path("user/register", RegisterView.as_view(), name="register"),
+    path("user/login", TokenObtainPairView.as_view(), name="get_token"),
+    path("user/current", CurrentUserView.as_view(), name="current_user"),
 ]
