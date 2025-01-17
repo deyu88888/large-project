2c56154ff69f843382092b735351f2b66b1e038a
Arham Zahid <146372787+arhamzahid23571113@users.noreply.github.com>
2025-01-17 01:12:57 +0000

    fix(urls): Ensure proper URL patterns and include `api` app routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,10 +4,17 @@ from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("admin", admin.site.urls),
+    # Admin Panel
+    path("admin/", admin.site.urls),
+
+    # User Management
     path("api/user/register", CreateUserView.as_view(), name="register"),
     path("api/user/login", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="refresh"),
-    path("api-auth", include("rest_framework.urls")),
+
+    # Django Rest Framework Auth
+    path("api-auth/", include("rest_framework.urls")),
+
+    # API Endpoints from `api` app
     path("api/", include("api.urls")),
 ]
