4737700196228550b82b099de9b3665c5dd1fb3f
m4nusha <manusharavindrathas@gmail.com>
2025-01-19 01:08:33 +0000

    Add user type fields and fix profile page

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -6,19 +6,38 @@ import { apiClient, apiPaths } from "../api";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
-  first_name: Yup.string().required("First name is required.").matches(/^[A-Za-z]+$/, "Shouldn't contain numerical or special characters.").max(50, "First name is too long."),
-  last_name: Yup.string().required("Last name is required.").matches(/^[A-Za-z]+$/, "Shouldn't contain numerical or special characters.").max(50, "Last name is too long."),
-  email: Yup.string().matches(    // checking email string with regular expression
-    /^[A-Z0-9._+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-    "Invalid email address."
-  ).required("Email is required.").max(50, "too long email id.").min(6, "too short email id."),
+  first_name: Yup.string()
+    .required("First name is required.")
+    .matches(
+      /^[A-Za-z]+$/,
+      "Shouldn't contain numerical or special characters."
+    )
+    .max(50, "First name is too long."),
+  last_name: Yup.string()
+    .required("Last name is required.")
+    .matches(
+      /^[A-Za-z]+$/,
+      "Shouldn't contain numerical or special characters."
+    )
+    .max(50, "Last name is too long."),
+  email: Yup.string()
+    .matches(
+      // checking email string with regular expression
+      /^[A-Z0-9._+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+      "Invalid email address."
+    )
+    .required("Email is required.")
+    .max(50, "too long email id.")
+    .min(6, "too short email id."),
 });
 
 export default function ProfilePage() {
   const navigate = useNavigate();
 
   const { user } = useAuthStore();
-  useEffect(() => { console.log(user) }, [])
+  useEffect(() => {
+    console.log(user);
+  }, []);
   if (user === null) {
     navigate("/login");
     return null;
@@ -32,18 +51,18 @@ export default function ProfilePage() {
       <div>role: {user.role}</div>
       <Formik
         initialValues={{
-          first_name: user?.first_name,
+          first_name: user?.firstName,
 
-          last_name: user.last_name,
+          last_name: user.lastName,
           username: user.username,
           email: user.email,
-          role: user.role
+          role: user.role,
         }}
         validationSchema={validationSchema} // validating the form
         onSubmit={async ({ first_name, last_name, username, email, role }) => {
           const res = await apiClient.put(apiPaths.USER.CURRENT, {
-            first_name: user.first_name == first_name ? undefined : first_name,
-            last_name: user.last_name == last_name ? undefined : last_name,
+            first_name: user.firstName == first_name ? undefined : first_name,
+            last_name: user.lastName == last_name ? undefined : last_name,
             username: user.username == username ? undefined : username,
             email: user.email == email ? undefined : email,
             role: user.role == role ? undefined : role,
@@ -53,26 +72,38 @@ export default function ProfilePage() {
       >
         <Form>
           <div>
-            <label htmlFor="first_name">First Name:  </label>
+            <label htmlFor="first_name">First Name: </label>
             <Field name="first_name" type="text" />
-            <ErrorMessage name="first_name" component="div" data-testid="error-first-name" />
+            <ErrorMessage
+              name="first_name"
+              component="div"
+              data-testid="error-first-name"
+            />
           </div>
 
           <div>
             <label htmlFor="last_name">Last Name</label>
             <Field name="last_name" type="text" />
-            <ErrorMessage name="last_name" component="div" data-testid="error-last-name" />
+            <ErrorMessage
+              name="last_name"
+              component="div"
+              data-testid="error-last-name"
+            />
           </div>
 
           <div>
             <label htmlFor="email">Email</label>
             <Field name="email" type="email" />
             <button type="submit">Update</button>
-            <ErrorMessage name="email" component="div" data-testid="error-email" />
+            <ErrorMessage
+              name="email"
+              component="div"
+              data-testid="error-email"
+            />
           </div>
         </Form>
       </Formik>
-      <div>verified: {user.is_active ? "true" : "false"}</div>
+      <div>verified: {user.isActive ? "true" : "false"}</div>
     </div>
   );
 }
