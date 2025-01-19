bbd260ef64eae6cd2e21f5d3f03e8a4d2f936a1f
m4nusha <manusharavindrathas@gmail.com>
2025-01-19 01:26:47 +0000

    Fix profile routing

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../stores/auth-store";
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -35,75 +34,96 @@ export default function ProfilePage() {
   const navigate = useNavigate();
 
   const { user } = useAuthStore();
-  useEffect(() => {
-    console.log(user);
-  }, []);
+
+  function goBack() {
+    navigate(-1);
+  }
+
   if (user === null) {
-    navigate("/login");
-    return null;
+    return (
+      <div className="container p-4">
+        <button className="btn bg-slate-400 mx-auto px-4 py-2" onClick={goBack}>
+          back
+        </button>
+
+        <p>No user found</p>
+      </div>
+    );
   }
 
   // TODO: add conditional rendering for student, based on whether they are presidents or not, after the models are updated
   return (
-    <div>
-      <h1>Profile</h1>
-      <div>username: {user.username}</div>
-      <div>role: {user.role}</div>
-      <Formik
-        initialValues={{
-          first_name: user?.firstName,
+    <div className="container p-4">
+      <button className="btn bg-slate-400 mx-auto px-4 py-2" onClick={goBack}>
+        back
+      </button>
 
-          last_name: user.lastName,
-          username: user.username,
-          email: user.email,
-          role: user.role,
-        }}
-        validationSchema={validationSchema} // validating the form
-        onSubmit={async ({ first_name, last_name, username, email, role }) => {
-          const res = await apiClient.put(apiPaths.USER.CURRENT, {
-            first_name: user.firstName == first_name ? undefined : first_name,
-            last_name: user.lastName == last_name ? undefined : last_name,
-            username: user.username == username ? undefined : username,
-            email: user.email == email ? undefined : email,
-            role: user.role == role ? undefined : role,
-          });
-          console.log(res);
-        }}
-      >
-        <Form>
-          <div>
-            <label htmlFor="first_name">First Name: </label>
-            <Field name="first_name" type="text" />
-            <ErrorMessage
-              name="first_name"
-              component="div"
-              data-testid="error-first-name"
-            />
-          </div>
+      <div>
+        <h1 className="text-2xl">Profile</h1>
+        <div>username: {user.username}</div>
+        <div>role: {user.role}</div>
+        <Formik
+          initialValues={{
+            first_name: user?.firstName,
 
-          <div>
-            <label htmlFor="last_name">Last Name</label>
-            <Field name="last_name" type="text" />
-            <ErrorMessage
-              name="last_name"
-              component="div"
-              data-testid="error-last-name"
-            />
-          </div>
+            last_name: user.lastName,
+            username: user.username,
+            email: user.email,
+            role: user.role,
+          }}
+          validationSchema={validationSchema} // validating the form
+          onSubmit={async ({
+            first_name,
+            last_name,
+            username,
+            email,
+            role,
+          }) => {
+            const res = await apiClient.put(apiPaths.USER.CURRENT, {
+              first_name: user.firstName == first_name ? undefined : first_name,
+              last_name: user.lastName == last_name ? undefined : last_name,
+              username: user.username == username ? undefined : username,
+              email: user.email == email ? undefined : email,
+              role: user.role == role ? undefined : role,
+            });
+            console.log(res);
+          }}
+        >
+          <Form>
+            <div>
+              <label htmlFor="first_name">First Name: </label>
+              <Field name="first_name" type="text" />
+              <ErrorMessage
+                name="first_name"
+                component="div"
+                data-testid="error-first-name"
+              />
+            </div>
 
-          <div>
-            <label htmlFor="email">Email</label>
-            <Field name="email" type="email" />
-            <button type="submit">Update</button>
-            <ErrorMessage
-              name="email"
-              component="div"
-              data-testid="error-email"
-            />
-          </div>
-        </Form>
-      </Formik>
-      <div>verified: {user.isActive ? "true" : "false"}</div>
+            <div>
+              <label htmlFor="last_name">Last Name</label>
+              <Field name="last_name" type="text" />
+              <ErrorMessage
+                name="last_name"
+                component="div"
+                data-testid="error-last-name"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email">Email</label>
+              <Field name="email" type="email" />
+              <button type="submit">Update</button>
+              <ErrorMessage
+                name="email"
+                component="div"
+                data-testid="error-email"
+              />
+            </div>
+          </Form>
+        </Formik>
+        <div>verified: {user.isActive ? "true" : "false"}</div>
+      </div>
     </div>
   );
 }
