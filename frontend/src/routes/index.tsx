2a4aaae0adcf002a2b3ab63521fbbc8b49fbdab8
m4nusha <manusharavindrathas@gmail.com>
2025-01-21 19:10:34 +0000

    Remove extra code

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -65,10 +65,6 @@ const routes = [
         path: "register",
         element: <RegisterPage />,
       },
-      {
-        path: "logout",
-        element: <Logout />,
-      },
     ],
   },
   {
