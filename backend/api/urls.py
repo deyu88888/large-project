ecced1a69cf412da03521425c065320a2531d3d9
Arham Zahid <146372787+arhamzahid23571113@users.noreply.github.com>
2025-01-23 06:12:48 +0000

    refactor(api): move /api-prefixed endpoints to `api/urls.py`

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from . import views
-from .views import RegisterView, CurrentUserView
+from .views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    # Authentication endpoints
-    path("user/token/refresh", TokenRefreshView.as_view(), name="refresh"),
-    path("user/register", RegisterView.as_view(), name="register"),  # Consolidated endpoint
+    # User Management
+    path("user/register", CreateUserView.as_view(), name="register"),
     path("user/login", TokenObtainPairView.as_view(), name="get_token"),
-    path("user/current", CurrentUserView.as_view(), name="current_user"),
-
-    # Dashboard
-    path('dashboard/stats/', views.dashboard_stats, name='dashboard_stats'),
+    path("token/refresh", TokenRefreshView.as_view(), name="refresh"),
 ]
