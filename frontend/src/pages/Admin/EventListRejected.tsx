7a5116ebcb4a473f477d080ac9710a4e163446c2
deyu88888 <k21081622@kcl.ac.uk>
2025-01-26 19:44:22 +0000

    fix profile page update
    fix profile page update
    Complete society request page
    
    improvement: redirect to admin home dashboard after creating a new admin
    notification on successful admin creation

---
+++ frontend/src/pages/Admin/EventListRejected.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { apiClient, apiPaths } from "../../api";
+
+type Event = { 
+    id: number; 
+    title: string; 
+    description: string; 
+    date: string; 
+    startTime: string; 
+    duration: string; 
+    hostedBy: number; 
+    location: string; 
+}
+
+const EventListRejected = () => {
+    const [events, setEvents] = useState<Event[]>([]);
+    useEffect(() => {
+
+    const getdata = async () => {
+        const res = await apiClient.get(apiPaths.USER.REJECTEDEVENTS);
+        console.log(res);
+        setEvents(res.data);
+    };
+    getdata();
+    },[]);
+
+    return (<div>
+        <table>
+            <tr>
+                <th>
+                    id</th>
+                <th>title</th>
+                <th>description</th>
+                <th>date</th>
+                <th>startTime</th>
+                <th>duration</th>
+                <th>hostedBy</th>
+                <th>location</th>
+            </tr>
+
+            {events.map((item, index) => {
+                return (
+                    <tr key={index}>
+                    <td>{item.id}</td>
+                    <td>{item.title}</td>
+                    <td>{item.description}</td>
+                    <td>{item.date}</td>
+                    <td>{item.startTime}</td>
+                    <td>{item.duration}</td>
+                    <td>{item.hostedBy}</td>
+                    <td>{item.location}</td>
+                    </tr>
+                )
+            })}
+
+            </table> </div>)
+} 
+
+export default EventListRejected;
