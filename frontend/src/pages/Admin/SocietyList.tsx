02be3f6968dc81a5c0112098bec22ff43b88f801
m4nusha <manusharavindrathas@gmail.com>
2025-01-27 00:28:33 +0000

    Add styling for admin dashboard

--- frontend/src/pages/Admin/SocietyList.tsx
+++ frontend/src/pages/Admin/SocietyList.tsx
@@ -21,6 +21,7 @@ const SocietyList = () => {
   const navigate = useNavigate();
   const [societies, setSocieties] = useState<Society[]>([]);
   const ws = useRef<WebSocket | null>(null);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
 
     const fetchSocieties = async () => {
         try {
@@ -31,6 +32,10 @@ const SocietyList = () => {
         }
     };
 
+
+    function goBack() {
+        navigate(-1);
+      }
 
   useEffect(() => {
     const connectWebSocket = () => {
