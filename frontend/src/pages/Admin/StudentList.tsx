a7a717217c7cdc0a6c31c1b45335d1b747efbd39
m4nusha <manusharavindrathas@gmail.com>
2025-01-27 01:09:11 +0000

    Fix error

--- frontend/src/pages/Admin/StudentList.tsx
+++ frontend/src/pages/Admin/StudentList.tsx
@@ -41,7 +41,7 @@ function StudentList() {
     return (
         <div className="min-h-screen bg-gray-100 py-8 px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center mb-6">
-            <h1 className="text-3xl font-bold text-gray-800 mb-6">Event List</h1>
+            <h1 className="text-3xl font-bold text-gray-800 mb-6">Student List</h1>
                 <button
                 className="group px-6 py-2.5 bg-gray-800 border border-gray-700 rounded-lg text-white hover:bg-gray-700 transition-all shadow-sm hover:shadow-md"
                 onClick={goBack}
