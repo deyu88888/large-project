4741d61d57454d5d769725b739c12c3e3ec1ff9e
m4nusha <manusharavindrathas@gmail.com>
2025-01-29 01:04:15 +0000

    Add styling for Manage Users

--- frontend/src/pages/Admin/AdminHome.tsx
+++ frontend/src/pages/Admin/AdminHome.tsx
@@ -34,57 +34,120 @@ export default function AdminHomePage() {
         <div className="flex justify-between items-center">
           <h1 className="text-lg font-bold">Admin Dashboard</h1>
           <div className="flex gap-6">
-            <button className=" hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(homeRef)}>Home</button>
-            <button className=" hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(userRef)}>Users</button>
-            <button className=" hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(societyRef)}>Societies</button>
-            <button className=" hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(eventRef)}>Events</button>
-            <button className=" hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(notificationRef)}>Notifications</button>
+            <button className="hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(homeRef)}>
+              Home
+            </button>
+            <button className="hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(userRef)}>
+              Users
+            </button>
+            <button className="hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(societyRef)}>
+              Societies
+            </button>
+            <button className="hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(eventRef)}>
+              Events
+            </button>
+            <button className="hover:bg-gray-300 rounded-md hover:text-slate-900 p-2 transition duration-300 ease-in-out" onClick={() => scrollToSection(notificationRef)}>
+              Notifications
+            </button>
           </div>
-          <button
-            onClick={logout}
-            className="px-4 py-2 bg-red-600 rounded-lg text-sm font-medium hover:bg-red-500 transition-all"
-          >
+          <button onClick={logout} className="px-4 py-2 bg-red-600 rounded-lg text-sm font-medium hover:bg-red-500 transition-all">
             Logout
           </button>
         </div>
       </nav>
 
       <section ref={homeRef} className="h-screen flex flex-col justify-center px-8">
-      <header className="h-screen flex flex-col justify-center items-center text-center px-8">
-        <h1 className="text-5xl font-extrabold text-gray-900 mb-4">
-          Welcome to your dashboard, {user?.first_name || "Admin"}!
-        </h1>
-        <p className="text-lg text-gray-600 mb-8">
-          Manage students, events, societies, and notifications with ease.
-        </p>
-        <Link
-          to="/profile"
-          className="px-8 py-3 bg-gray-800 text-white rounded-lg hover:bg-slate-500 transition-all"
-        >
-          View Profile
-        </Link>
-      </header>
+        <header className="h-screen flex flex-col justify-center items-center text-center px-8">
+          <h1 className="text-5xl font-extrabold text-gray-900 mb-4">
+            Welcome to your dashboard, {user?.first_name || "Admin"}!
+          </h1>
+          <p className="text-lg text-gray-600 mb-8">
+            Manage students, events, societies, and notifications with ease.
+          </p>
+          <Link
+            to="/profile"
+            className="px-8 py-3 bg-gray-800 text-white rounded-lg hover:bg-slate-500 transition-all"
+          >
+            View Profile
+          </Link>
+        </header>
       </section>
 
-      <section ref={userRef} className="h-screen flex flex-col justify-center px-8">
+      <section ref={userRef} className="min-h-screen flex flex-col px-7 pt-[100px] pb-6">
         <div className="flex justify-between items-center mb-8">
           <h2 className="text-3xl font-bold flex items-center">
             <FaUsers className="text-green-500 mr-3" />
             Manage Users
           </h2>
-          <Link
-            to="/student-list"
-            className="text-blue-500 hover:underline font-medium"
-          >
-            View All Students
-          </Link>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="p-6 bg-white rounded-xl shadow hover:shadow-lg border border-gray-200 transition-transform hover:-translate-y-1">
-            <h3 className="text-xl font-semibold text-gray-900 mb-4">Student 1</h3>
-            <button className="bg-red-500 text-white px-6 py-2 rounded-lg hover:bg-red-600 transition-all">
-              Remove User
-            </button>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 flex-grow">
+          <div className="p-6 bg-indigo-50 rounded-xl border border-gray-200 flex flex-col items-start h-full">
+            <div className="flex items-center justify-between w-full mb-4">
+              <h3 className="text-2xl font-semibold text-gray-900">Students</h3>
+              <Link
+                to="/student-list"
+                className="text-blue-500 hover:underline font-medium"
+              >
+                Manage All Students
+              </Link>
+            </div>
+
+            <div className="p-6 bg-white rounded-lg border border-gray-200 w-full">
+              <h4 className="text-lg font-medium text-gray-800 mb-4">Student Requests</h4>
+
+              <ul className="space-y-3">
+                <li className="p-3 border rounded-lg bg-gray-50 flex justify-between items-center shadow hover:shadow-md transition-transform hover:-translate-y-0">
+                  <span className="text-sm text-gray-700">
+                    Request 1: Report misuse of platform
+                  </span>
+                  <button className="text-red-500 text-sm font-medium hover:underline">
+                    Resolve
+                  </button>
+                </li>
+                <li className="p-3 border rounded-lg bg-gray-50 flex justify-between items-center shadow hover:shadow-md transition-transform hover:-translate-y-0">
+                  <span className="text-sm text-gray-700">
+                    Request 2: Help with register
+                  </span>
+                  <button className="text-red-500 text-sm font-medium hover:underline">
+                    Resolve
+                  </button>
+                </li>
+                <li className="p-3 border rounded-lg bg-gray-50 flex justify-between items-center shadow hover:shadow-md transition-transform hover:-translate-y-0">
+                  <span className="text-sm text-gray-700">
+                    Request 3: Reporting a bug in the system
+                  </span>
+                  <button className="text-red-500 text-sm font-medium hover:underline">
+                    Resolve
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="p-6 bg-indigo-50 rounded-xl border border-gray-200 flex flex-col items-start h-full">
+            <div className="flex items-center justify-between w-full mb-4">
+              <h3 className="text-2xl font-semibold text-gray-900">Admins</h3>
+              <Link
+                to="/admin-list"
+                className="text-blue-500 hover:underline font-medium"
+              >
+                Manage All Admins
+              </Link>
+            </div>
+            <div className="p-6 bg-white rounded-lg border border-gray-200 w-full mb-4">
+              <h4 className="text-lg font-medium text-gray-800 mb-4">Recent Activity Log</h4>
+            </div>
+            <div className="p-6 bg-white rounded-lg border border-gray-200 w-full mb-4">
+              <h4 className="text-lg font-medium text-gray-800 mb-4">Messages</h4>
+            </div>
+            <div className="p-6 bg-white rounded-xl border border-gray-200 mb-4">
+              <Link to="/create-admin">
+                <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition-transform hover:-translate-y-0 shadow hover:shadow-lg">
+                  Create Admin
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
--- frontend/src/pages/Admin/CreateAdmin.tsx
+++ frontend/src/pages/Admin/CreateAdmin.tsx
@@ -1,13 +1,15 @@
 import { apiClient, apiPaths } from "../../api";
 import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import CircularLoader from "../../components/loading/circular-loader";
 import { useFormik } from "formik";
 
 export default function CreateAdmin() {
   const [loading, setLoading] = useState(false);
-//   const navigate = useNavigate();
-
+  const navigate = useNavigate();
+  function goBack() {
+    navigate(-1);
+  }
   const createAminFormik = useFormik({
     initialValues: {
       username: "",
@@ -30,109 +32,127 @@ export default function CreateAdmin() {
   });
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 py-6 px-4 sm:px-6 lg:px-8">
-      <form
-        onSubmit={createAminFormik.handleSubmit}
-        className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg overflow-auto"
+    <div className="relative min-h-screen bg-gray-100 py-6 px-4 sm:px-6 lg:px-8 flex items-center justify-center">
+      <button
+        className="group px-6 py-2.5 bg-gray-800 border border-gray-700 rounded-lg text-white hover:bg-gray-700 transition-all shadow-sm hover:shadow-md absolute top-4 right-4"
+        onClick={goBack}
       >
-        <h1 className="text-2xl font-bold text-center mb-6">Create A New Admin</h1>
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Username
-          </label>
-          <input
-            id="username"
-            type="text"
-            value={createAminFormik.values.username}
-            onChange={createAminFormik.handleChange}
-            placeholder="Enter your username"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="firstName"
-            className="block text-sm font-medium text-gray-700"
-          >
-            First Name
-          </label>
-          <input
-            id="firstName"
-            type="text"
-            value={createAminFormik.values.firstName}
-            onChange={createAminFormik.handleChange}
-            placeholder="Enter your first name"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="lastName"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Last Name
-          </label>
-          <input
-            id="lastName"
-            type="text"
-            value={createAminFormik.values.lastName}
-            onChange={createAminFormik.handleChange}
-            placeholder="Enter your last name"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            E-mail
-          </label>
-          <input
-            id="email"
-            type="text"
-            value={createAminFormik.values.email}
-            onChange={createAminFormik.handleChange}
-            placeholder="Enter your e-mail"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            value={createAminFormik.values.password}
-            onChange={createAminFormik.handleChange}
-            placeholder="Enter your password"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-            required
-          />
-        </div>
-        {loading && (
-          <div className="flex justify-center mb-4">
-            <CircularLoader />
-          </div>
-        )}
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow hover:bg-indigo-700"
+        <span className="inline-flex items-center">
+          <span className="mr-2 group-hover:-translate-x-1 transition-transform duration-200">←</span>
+          Back
+        </span>
+      </button>
+
+      <div className="w-full max-w-md">
+        <form
+          onSubmit={createAminFormik.handleSubmit}
+          className="bg-white p-6 rounded-lg shadow-lg overflow-auto"
         >
-          Create a New Admin
-        </button>
-      </form>
+          <h1 className="text-2xl font-bold text-center mb-6">Create A New Admin</h1>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Username
+            </label>
+            <input
+              id="username"
+              type="text"
+              value={createAminFormik.values.username}
+              onChange={createAminFormik.handleChange}
+              placeholder="Enter your username"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="firstName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              First Name
+            </label>
+            <input
+              id="firstName"
+              type="text"
+              value={createAminFormik.values.firstName}
+              onChange={createAminFormik.handleChange}
+              placeholder="Enter your first name"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="lastName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Last Name
+            </label>
+            <input
+              id="lastName"
+              type="text"
+              value={createAminFormik.values.lastName}
+              onChange={createAminFormik.handleChange}
+              placeholder="Enter your last name"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              E-mail
+            </label>
+            <input
+              id="email"
+              type="text"
+              value={createAminFormik.values.email}
+              onChange={createAminFormik.handleChange}
+              placeholder="Enter your e-mail"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              value={createAminFormik.values.password}
+              onChange={createAminFormik.handleChange}
+              placeholder="Enter your password"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+              required
+            />
+          </div>
+
+          {loading && (
+            <div className="flex justify-center mb-4">
+              <CircularLoader />
+            </div>
+          )}
+          
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow hover:bg-indigo-700"
+          >
+            Create a New Admin
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
