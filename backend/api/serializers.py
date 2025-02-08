f486b5e773e23ddab3dbcab924e20132b3d7c7d2
ThomasHolden-eng <thomasholden2005@gmail.com>
2025-02-08 13:49:30 +0000

    Add docstrings to award serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -544,11 +544,17 @@ class EventCalendarSerializer(serializers.ModelSerializer):
 
 
 class AwardSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Award model
+    """
     class Meta:
         model = Award
         fields = '__all__'
 
 class AwardStudentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the AwardStudent model
+    """
     award = AwardSerializer(read_only=True)
     student_id = serializers.PrimaryKeyRelatedField(source='student', queryset=Student.objects.all(), write_only=True)
     award_id = serializers.PrimaryKeyRelatedField(source='award', queryset=Award.objects.all(), write_only=True)
