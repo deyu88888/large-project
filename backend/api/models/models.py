0c4d0409adef9791c780d92c0c09aaf163948492
ThomasHolden-eng <thomasholden2005@gmail.com>
2025-03-22 15:54:00 +0000

    Add award_models.py

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -15,7 +15,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-
 def validate_social_media_links(value):
     """
     Validate the social_media_links JSON field.
@@ -482,64 +481,6 @@ class SiteSettings(models.Model):
         """
         obj, created = cls.objects.get_or_create(pk=cls.singleton_instance_id)
         return obj
-
-
-class Award(models.Model):
-    """
-    Awards to be granted to students
-    """
-    RANKS = [
-        ("Bronze", "Bronze"),
-        ("Silver", "Silver"),
-        ("Gold", "Gold"),
-    ]
-    rank = models.CharField(
-        max_length=10,
-        default="Bronze",
-        blank=False,
-        null=False,
-        choices=RANKS,
-    )
-    is_custom = models.BooleanField(default=False)
-    title = models.CharField(
-        max_length=20,
-        default="default_title",
-        blank=False,
-        null=False,
-    )
-    description = models.CharField(
-        max_length=150,
-        default="default_description",
-        blank=False,
-        null=False,
-    )
-
-    def __str__(self):
-        return f"{self.title}, {self.rank}"
-
-
-class AwardStudent(models.Model):
-    """
-    The relation between Award and Student
-    """
-    award = models.ForeignKey(
-        "Award",
-        on_delete=models.CASCADE,
-        related_name="student_awards",
-        blank=False,
-        null=False,
-    )
-    student = models.ForeignKey(
-        "Student",
-        on_delete=models.CASCADE,
-        related_name="award_students",
-        blank=False,
-        null=False,
-    )
-    awarded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.student}, ({self.award})"
 
 
 class NewsNotification(models.Model):
